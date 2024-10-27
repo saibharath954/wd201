474751cb5ee7a2cd6f7b6a91bd477e37c74ac5e6
Sai Bharath <saibharath1675@gmail.com>
2024-10-27 22:56:40 +0530

    Initial commit

--- todo-app/index.js
+++ todo-app/index.js
@@ -1,10 +1,5 @@
-const express = require('express');
-const app = express();
-
-app.get("/", (request, response) => {
-    response.send("hello world")
-});
+const app = require("./app");
 
 app.listen(3000, () => {
-    console.log("Started express server at port 3000")
-})
+  console.log("Started express server at port 3000");
+});
